2dbb6c20607edda5383f3e2078338aa2975bf1ff
XiangLuc <xiangluc01@gmail.com>
2024-04-04 16:30:47 +0200

    création d'un singleton, et rangement dans le code de joueur_routes.py

--- joueur_routes.py
+++ joueur_routes.py
@@ -27,67 +27,60 @@ joueur_schema = {
     'required': ['nom', 'prenom', 'categorie', 'sexe', 'point']
 }
 
+db_joueur = Mongo2Client().db['joueur']
+
 
 @joueurs_bp.route('/', methods=['GET'])
 def get_all_joueurs():
-    with Mongo2Client() as mongo_client:
-        db_joueur = mongo_client.db['joueur']
-        joueurs = db_joueur.find()
-        joueurs_list = []
-        for joueur in joueurs:
-            joueur['_id'] = str(joueur['_id'])
-            joueurs_list.append(joueur)
-        return jsonify(joueurs_list)
+    joueurs = db_joueur.find()
+    joueurs_list = []
+    for joueur in joueurs:
+        joueur['_id'] = str(joueur['_id'])
+        joueurs_list.append(joueur)
+    return jsonify(joueurs_list)
 
 
 @joueurs_bp.route('/<string:id_joueur>', methods=['GET'])
 def get_joueur_by_id(id_joueur):
-    with Mongo2Client() as mongo_client:
-        db_joueur = mongo_client.db['joueur']
-        joueur = db_joueur.find_one({'_id': ObjectId(id_joueur)})
-        if joueur:
-            joueur['_id'] = str(joueur['_id'])
-            return jsonify(joueur)
-        else:
-            return jsonify({'erreur': f"le joueur d'identifiant {id_joueur} n'existe pas."}), 404
+    joueur = db_joueur.find_one({'_id': ObjectId(id_joueur)})
+    if joueur:
+        joueur['_id'] = str(joueur['_id'])
+        return jsonify(joueur)
+    else:
+        return jsonify({'erreur': f"le joueur d'identifiant {id_joueur} n'existe pas."}), 404
 
 
 @joueurs_bp.route('/add', methods=['POST'])
 @expects_json(joueur_schema)
 def add_joueur():
     data = request.json
-    with Mongo2Client() as mongo_client:
-        db_joueur = mongo_client.db['joueur']
-        insert_joueur = db_joueur.insert_one(data)
-        if insert_joueur:
-            return jsonify({"message": "La requete a bien été insérée"})
-        else:
-            return jsonify({"message": "Erreur lors de l'insertion"}), 404
+    insert_joueur = db_joueur.insert_one(data)
+    if insert_joueur:
+        return jsonify({"message": "La requete a bien été insérée"})
+    else:
+        return jsonify({"message": "Erreur lors de l'insertion"}), 404
 
 
 @joueurs_bp.route('/<string:id_joueur>', methods=['DELETE'])
 def delete_joueur_by_id(id_joueur):
-    with Mongo2Client() as mongo_client:
-        db_joueur = mongo_client.db['joueur']
-        delete_joueur = db_joueur.delete_one({'_id': ObjectId(id_joueur)})
-
-        if delete_joueur:
-            return jsonify({"message": "La suppression a bien été réalisée."})
-        else:
-            return jsonify({'message': 'Erreur lors de la suppression'}), 404
+    delete_joueur = db_joueur.delete_one({'_id': ObjectId(id_joueur)})
+    if delete_joueur:
+        return jsonify({"message": "La suppression a bien été réalisée."})
+    else:
+        return jsonify({'message': 'Erreur lors de la suppression'}), 404
 
 
 @joueurs_bp.route('/<string:id_joueur>', methods=['PUT'])
 def update_joueur_by_id(id_joueur):
     data = request.json
-    with Mongo2Client() as mongo_client:
-        db_joueur = mongo_client.db['joueur']
-        update_joueur = db_joueur.update_one({'_id': ObjectId(id_joueur)}, {'$set': data})
 
-        if update_joueur.modified_count > 0:
-            return jsonify({"message": "La mise à jour a bien été réalisée."})
-        else:
-            return jsonify({'message': 'Erreur lors de la mise à jour'}), 404
+    update_joueur = db_joueur.update_one({'_id': ObjectId(id_joueur)}, {'$set': data})
+
+    if update_joueur.modified_count > 0:
+        return jsonify({"message": "La mise à jour a bien été réalisée."})
+    else:
+        return jsonify({'message': 'Erreur lors de la mise à jour'}), 404
+
 
 @joueurs_bp.route('/add_fichier', methods=['POST'])
 def add_joueurs_fichier():
@@ -138,7 +131,8 @@ def add_joueurs_fichier():
         mongo_client.close()
         return jsonify({"succès": False, "message": "Erreur lors de l'insertion des joueurs", "erreur": str(e)}), 500
 
-@joueurs_bp.route('/filtre', methods=['GET'])
+
+@joueurs_bp.route('/', methods=['GET'])
 def get_joueurs_filtre():
     niveau = request.args.get('niveau', 'Mixte')
 
@@ -147,12 +141,9 @@ def get_joueurs_filtre():
     if niveau != 'Mixte':
         query['categorie.niveau'] = niveau
 
-    with Mongo2Client() as mongo_client:
-        db_joueur = mongo_client.db['joueur']
-        joueurs = db_joueur.find(query)
-        joueurs_list = []
-        for joueur in joueurs:
-            joueur['_id'] = str(joueur['_id'])
-            joueurs_list.append(joueur)
-        return jsonify(joueurs_list)
-
+    joueurs = db_joueur.find(query)
+    joueurs_list = []
+    for joueur in joueurs:
+        joueur['_id'] = str(joueur['_id'])
+        joueurs_list.append(joueur)
+    return jsonify(joueurs_list)
--- mongo_client.py
+++ mongo_client.py
@@ -2,9 +2,18 @@ from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 
 
-class Mongo2Client:
+class SingletonMeta(type):
+    _instances = {}
 
-    def __init__(self, host='localhost', port=27017, db_name='Tournoi_Ping-pong', username='juba', password=None):
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class Mongo2Client(metaclass=SingletonMeta):
+    def __init__(self, host='localhost', port=27017, db_name='Tournoi_Ping-pong', username='luc', password=None):
         try:
             if username and password:
                 uri = f"mongodb://{username}:{password}@{host}:{port}/{db_name}"
@@ -15,8 +24,13 @@ class Mongo2Client:
         except ConnectionFailure as e:
             print("Erreur de connexion à la base de données MongoDB:", e)
 
-    def __enter__(self):
-        return self
 
-    def __exit__(self, type, value, traceback):
-        self.client.close()
+if __name__ == "__main__":
+    # Test du singleton MongoDB client.
+    client1 = Mongo2Client()
+    client2 = Mongo2Client()
+
+    if id(client1) == id(client2):
+        print("Singleton works, both variables contain the same instance.")
+    else:
+        print("Singleton failed, variables contain different instances.")
